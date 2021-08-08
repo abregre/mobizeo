39bcc297814fb7f351a6d3a088f33df971568c64
V.Paparidis <v.paparidis@gmail.com>
2021-08-08 16:58:42 +0300

    handle server errors

--- backend/server.js
+++ backend/server.js
@@ -2,24 +2,23 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import productRoutes from './routes/productRoutes.js'
 dotenv.config()
+
 
 connectDB()
 
 const app = express()
-
-import products from './data/products.js'
-
 const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
     res.send('Server is running')
 })
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product)
-})
+
+app.use('/api/products', productRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
+
 app.listen(PORT, console.log(colors.yellow.bold(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)))
